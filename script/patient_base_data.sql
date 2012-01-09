795b4afe21bf1e27f27ec9da3f97b507241dedb3
Catchit <precious.bondwe@baobabhealth.org>
2012-01-09 17:15:39 +0200

    made some modifications to the script

--- script/patient_base_data.sql
+++ script/patient_base_data.sql
@@ -1,143 +1,84 @@
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+UPDATE healthdata.MasterPatientRecord
+SET Birth_Date = CONCAT_WS('-', SUBSTRING(Birth_Date,8,4),CASE SUBSTRING(Birth_Date,4,3) WHEN 'JAN' THEN '01' WHEN 'FEB' THEN '02' WHEN 'MAR' THEN '03' WHEN 'APR' THEN '04' WHEN 'MAY' THEN '05' WHEN 'JUN' THEN '06' WHEN 'JUL' THEN '07' WHEN 'AUG' THEN '08' WHEN 'SEP' THEN '09' WHEN 'OCT' THEN '10' WHEN 'NOV' THEN '11' WHEN 'DEC' THEN '12' END, SUBSTRING(Birth_Date,1,2)),
+    Date_Reg = CONCAT_WS('-', SUBSTRING(Date_Reg,8,4),CASE SUBSTRING(Date_Reg,4,3) WHEN 'JAN' THEN '01' WHEN 'FEB' THEN '02' WHEN 'MAR' THEN '03' WHEN 'APR' THEN '04' WHEN 'MAY' THEN '05' WHEN 'JUN' THEN '06' WHEN 'JUL' THEN '07' WHEN 'AUG' THEN '08' WHEN 'SEP' THEN '09' WHEN 'OCT' THEN '10' WHEN 'NOV' THEN '11' WHEN 'DEC' THEN '12' END, SUBSTRING(Date_Reg,1,2));
 
+/*End of update*/
 
-/* Update users */
-INSERT INTO openmrs_b2.users (user_id, username, password, salt, secret_answer, secret_question, creator, retired, retired_by, retire_reason, date_retired, date_created, uuid)
-SELECT user_id, username, password, salt, secret_answer, secret_question, 1 AS creator, voided, voided_by, void_reason, date_voided, date_created, (SELECT UUID()) AS uuid FROM openmrs_bart1.users WHERE user_id > 1;
+INSERT INTO openmrs_bart2.person (birthdate, birthdate_estimated, gender, death_date, creator, voided, voided_by, void_reason, date_voided, date_created, uuid)
+SELECT DATE(Birth_Date), 0, Sex, NULL, 1, Site_ID, NULL, Pat_ID, NULL, DATE(Date_Reg), (SELECT UUID()) AS uuid FROM healthdata.MasterPatientRecord WHERE Pat_ID > 1;
 
-UPDATE openmrs_bart1.role SET role = "Provider" WHERE role = "provider";
-UPDATE openmrs_bart1.role SET role = "Superuser" WHERE role = "superuser";
+/*UPDATE openmrs_b2.person SET gender = LEFT(RTRIM(LTRIM(gender)), 1);*/
 
-DELETE FROM openmrs_b2.user_role;
-INSERT INTO openmrs_b2.user_role (user_id, role)
-SELECT ur.user_id, r.role FROM openmrs_bart1.user_role ur LEFT JOIN openmrs_bart1.role r ON ur.role_id = r.role_id WHERE ur.user_id IN (SELECT user_id FROM openmrs_bart1.users) AND r.role NOT IN ('Data Entry Clerk') ;
-
-INSERT INTO openmrs_b2.user_role (user_id, role)
-SELECT ur.user_id, 'Data Assistant' FROM openmrs_bart1.user_role ur LEFT JOIN openmrs_bart1.role r ON ur.role_id = r.role_id WHERE ur.user_id IN (SELECT user_id FROM openmrs_bart1.users) AND r.role IN ("Data Entry Clerk") ;
-
-/* Update locations 
-DELETE FROM openmrs_b2.location_tag_map;
-DELETE FROM openmrs_b2.location;
-
-INSERT INTO openmrs_b2.location (location_id, name, description, address1, address2, city_village, country, postal_code, creator, date_created, uuid)
-SELECT location_id, name, description, address1, address2, city_village, country, postal_code, creator, date_created, (SELECT UUID()) AS uuid FROM openmrs_bart1.location WHERE location_id < 1000;
-
-INSERT INTO openmrs_b2.location (name, description, creator, date_created, uuid)
-	VALUES ('Registration', 'Workstation Location', 1, (NOW()), (SELECT UUID())),
-	('Vitals', 'Workstation Location', 1, (NOW()), (SELECT UUID())),
-	('Outpatient', 'Workstation Location', 1, (NOW()), (SELECT UUID())),
-	('HIV Reception', 'Workstation Location', 1, (NOW()), (SELECT UUID())),
-	('HIV Clinician Station', 'Workstation Location', 1, (NOW()), (SELECT UUID())),
-	('HIV Nurse Station', 'Workstation Location', 1, (NOW()), (SELECT UUID())),
-	('Chronic Cough', 'Workstation Location', 1, (NOW()), (SELECT UUID())),
-	('TB Reception', 'Workstation Location', 1, (NOW()), (SELECT UUID())),
-	('TB Sputum Submission Station', 'Workstation Location', 1, (NOW()), (SELECT UUID()));
-
-INSERT INTO `location_tag_map` (`location_id`, `location_tag_id`) VALUES
-	((SELECT location_id FROM location WHERE name = 'Registration'),(SELECT location_tag_id FROM location_tag WHERE name = "Workstation Location") );
-INSERT INTO `location_tag_map` (`location_id`, `location_tag_id`) VALUES
-	((SELECT location_id FROM location WHERE name = 'Vitals'),(SELECT location_tag_id FROM location_tag WHERE name = "Workstation Location") );
-INSERT INTO `location_tag_map` (`location_id`, `location_tag_id`) VALUES
-	((SELECT location_id FROM location WHERE name = 'Outpatient'),(SELECT location_tag_id FROM location_tag WHERE name = "Workstation Location") );
-INSERT INTO `location_tag_map` (`location_id`, `location_tag_id`) VALUES
-	((SELECT location_id FROM location WHERE name = 'HIV Reception'),(SELECT location_tag_id FROM location_tag WHERE name = "Workstation Location") );
-INSERT INTO `location_tag_map` (`location_id`, `location_tag_id`) VALUES
-	((SELECT location_id FROM location WHERE name = 'HIV Clinician Station'),(SELECT location_tag_id FROM location_tag WHERE name = "Workstation Location") );
-INSERT INTO `location_tag_map` (`location_id`, `location_tag_id`) VALUES
-	((SELECT location_id FROM location WHERE name = 'HIV Nurse Station'),(SELECT location_tag_id FROM location_tag WHERE name = "Workstation Location") );
-INSERT INTO `location_tag_map` (`location_id`, `location_tag_id`) VALUES
-	((SELECT location_id FROM location WHERE name = 'Chronic Cough'),(SELECT location_tag_id FROM location_tag WHERE name = "Workstation Location") );
-INSERT INTO `location_tag_map` (`location_id`, `location_tag_id`) VALUES
-	((SELECT location_id FROM location WHERE name = 'TB Reception'),(SELECT location_tag_id FROM location_tag WHERE name = "Workstation Location") );
-INSERT INTO `location_tag_map` (`location_id`, `location_tag_id`) VALUES
-	((SELECT location_id FROM location WHERE name = 'TB Sputum Submission Station'),(SELECT location_tag_id FROM location_tag WHERE name = "Workstation Location") );
-*/
-INSERT INTO openmrs_b2.location (location_id, name, description, address1, address2, city_village, country, postal_code, creator, date_created, uuid)
-SELECT location_id, name, description, address1, address2, city_village, country, postal_code, creator, date_created, (SELECT UUID()) AS uuid FROM openmrs_bart1.location WHERE location_id > 999;
-
-/* Update patient and person details */
-INSERT INTO openmrs_b2.person (person_id, birthdate, birthdate_estimated, gender, death_date, creator, voided, voided_by, void_reason, date_voided, date_created, uuid)
-SELECT patient_id, birthdate, birthdate_estimated, gender, death_date, creator, voided, voided_by, void_reason, date_voided, date_created, (SELECT UUID()) AS uuid FROM openmrs_bart1.patient;
-
-UPDATE openmrs_b2.person SET gender = LEFT(RTRIM(LTRIM(gender)), 1);
-
-INSERT INTO openmrs_b2.patient (patient_id, creator, voided, voided_by, void_reason, date_voided, date_created)
-SELECT patient_id, creator, voided, voided_by, void_reason, date_voided, date_created FROM openmrs_bart1.patient;
+INSERT INTO openmrs_bart2.patient (patient_id, creator, voided, voided_by, void_reason, date_voided, date_created)
+SELECT person_id, creator, voided, voided_by, void_reason, date_voided, date_created FROM openmrs_bart2.person where openmrs_bart2.person.person_id > 1;
 
 /* Update patient person addresses */
-INSERT INTO openmrs_b2.person_address (person_id, city_village, creator, voided, voided_by, void_reason, date_voided, date_created, uuid)
-SELECT patient_id, city_village, creator, voided, voided_by, void_reason, date_voided, date_created, (SELECT UUID()) AS uuid FROM openmrs_bart1.patient_address;
+INSERT INTO openmrs_bart2.person_address (person_id, city_village, creator, voided, voided_by, void_reason, date_voided, date_created, uuid)
+SELECT p.person_id,LTRIM(RTRIM(RIGHT(mpr.address, LENGTH(mpr.address) - INSTR(mpr.address,'/')))) AS city_village,p.creator,p.voided, p.voided_by,'' AS void_reason, p.date_voided, p.date_created, (SELECT UUID()) AS uuid
+FROM openmrs_bart2.person p 
+ 	INNER JOIN healthdata.MasterPatientRecord mpr
+	ON mpr.Pat_ID = p.void_reason 
+	AND mpr.Site_ID = p.voided;
 
-/* Update patient identifiers and attributes */
-INSERT INTO openmrs_b2.patient_identifier (patient_id, identifier_type, preferred, location_id, identifier, creator, voided, voided_by, void_reason, date_voided, date_created, uuid)
-SELECT patient_id, 3 AS identifier_type, preferred, location_id, identifier, creator, voided, voided_by, void_reason, date_voided, date_created, (SELECT UUID()) AS uuid FROM openmrs_bart1.patient_identifier WHERE identifier_type = 1;
-
-INSERT INTO openmrs_b2.patient_identifier (patient_id, identifier_type, preferred, location_id, identifier, creator, voided, voided_by, void_reason, date_voided, date_created, uuid)
-SELECT patient_id, 17 AS identifier_type, preferred, location_id, identifier, creator, voided, voided_by, void_reason, date_voided, date_created, (SELECT UUID()) AS uuid FROM openmrs_bart1.patient_identifier WHERE identifier_type = 10;
-
-INSERT INTO openmrs_b2.patient_identifier (patient_id, identifier_type, preferred, location_id, identifier, creator, voided, voided_by, void_reason, date_voided, date_created, uuid)
-SELECT patient_id, 4 AS identifier_type, preferred, location_id, identifier, creator, voided, voided_by, void_reason, date_voided, date_created, (SELECT UUID()) AS uuid FROM openmrs_bart1.patient_identifier WHERE identifier_type = 18;
+/* Update patient identifiers and attributes 'skipping them for now'*/
 
 INSERT INTO openmrs_b2.patient_identifier (patient_id, identifier_type, preferred, location_id, identifier, creator, voided, voided_by, void_reason, date_voided, date_created, uuid)
-SELECT patient_id, 18 AS identifier_type, preferred, location_id, identifier, creator, voided, voided_by, void_reason, date_voided, date_created, (SELECT UUID()) AS uuid FROM openmrs_bart1.patient_identifier WHERE identifier_type = 19;
+SELECT 	DISTINCT
+	p.person_id,
+	3 AS identifier_type,
+	0 AS preffered,
+	CASE mpr.Site_ID
+		WHEN 101 THEN 295
+		WHEN 102 THEN 614
+		WHEN 103 THEN 703
+		ELSE 1
+	END AS location_id,
+	rs.Patient_Identifier AS identifier,
+	1 AS creator, 0 AS voided, 0 AS voided_by, '' as void_reason, NULL AS date_voided, p.date_created
+FROM healthdata.RadiologyStudy rs
+	INNER JOIN healthdata.MasterPatientRecord mpr
+		ON CASE LEFT(rs.Patient_Identifier, 1)
+			WHEN "p" THEN SUBSTRING(rs.Patient_Identifier,3,(LENGTH(rs.Patient_Identifier) - 5))
+			ELSE REPLACE(SUBSTRING(Patient_Identifier,5,7), "-","")
+		   END = mpr.Pat_ID
+	INNER JOIN openmrs_bart2.person p
+		ON mpr.Site_ID = p.voided
+		AND mpr.Pat_ID = p.void_reason;
 
-INSERT INTO openmrs_b2.person_attribute (person_id, person_attribute_type_id, value, creator, voided, voided_by, void_reason, date_voided, date_created, uuid)
-SELECT patient_id, 13 AS identifier_type, identifier, creator, voided, voided_by, void_reason, date_voided, date_created, (SELECT UUID()) AS uuid FROM openmrs_bart1.patient_identifier WHERE identifier_type = 3;
+/*Radiology Study Number*/
 
-INSERT INTO openmrs_b2.person_attribute (person_id, person_attribute_type_id, value, creator, voided, voided_by, void_reason, date_voided, date_created, uuid)
-SELECT patient_id, 12 AS identifier_type, identifier, creator, voided, voided_by, void_reason, date_voided, date_created, (SELECT UUID()) AS uuid FROM openmrs_bart1.patient_identifier WHERE identifier_type = 5;
+INSERT INTO openmrs_b2.patient_identifier (patient_id, identifier_type, preferred, location_id, identifier, creator, voided, voided_by, void_reason, date_voided, date_created, uuid)
+SELECT 	DISTINCT
+	p.person_id,
+	30 AS identifier_type,
+	0 AS preffered,
+	CASE mpr.Site_ID
+		WHEN 101 THEN 295
+		WHEN 102 THEN 614
+		WHEN 103 THEN 703
+		ELSE 1
+	END AS location_id,
+	rs.Study_Number AS identifier,
+	1 AS creator, 0 AS voided, 0 AS voided_by, '' as void_reason, NULL AS date_voided, p.date_created
+FROM healthdata.RadiologyStudy rs
+	INNER JOIN healthdata.MasterPatientRecord mpr
+		ON CASE LEFT(rs.Patient_Identifier, 1)
+			WHEN "p" THEN SUBSTRING(rs.Patient_Identifier,3,(LENGTH(rs.Patient_Identifier) - 5))
+			ELSE REPLACE(SUBSTRING(Patient_Identifier,5,7), "-","")
+		  END = mpr.Pat_ID
+	INNER JOIN openmrs_bart2.person p
+		ON mpr.Site_ID = p.voided
+		AND mpr.Pat_ID = p.void_reason;
 
-INSERT INTO openmrs_b2.person_attribute (person_id, person_attribute_type_id, value, creator, voided, voided_by, void_reason, date_voided, date_created, uuid)
-SELECT patient_id, 14 AS identifier_type, identifier, creator, voided, voided_by, void_reason, date_voided, date_created, (SELECT UUID()) AS uuid FROM openmrs_bart1.patient_identifier WHERE identifier_type = 11;
-
-INSERT INTO openmrs_b2.person_attribute (person_id, person_attribute_type_id, value, creator, voided, voided_by, void_reason, date_voided, date_created, uuid)
-SELECT patient_id, 15 AS identifier_type, identifier, creator, voided, voided_by, void_reason, date_voided, date_created, (SELECT UUID()) AS uuid FROM openmrs_bart1.patient_identifier WHERE identifier_type = 12;
-
-/* Update patient person names */
-INSERT INTO openmrs_b2.person_name (person_id, middle_name, given_name, family_name, preferred, prefix, creator, voided, voided_by, void_reason, date_voided, date_created, uuid)
-SELECT patient_id, middle_name, given_name, family_name, preferred, prefix, creator, voided, voided_by, void_reason, date_voided, date_created, (SELECT UUID()) AS uuid FROM openmrs_bart1.patient_name;
-
-/* Update users persons */
-SET @'max_person_id' = (SELECT MAX(person_id) FROM openmrs_b2.person);
-
-INSERT INTO openmrs_b2.person (person_id, birthdate, birthdate_estimated, gender, death_date, creator, voided, voided_by, void_reason, date_voided, date_created, uuid)
-SELECT (user_id + @'max_person_id') AS user_id, NOW(), 0, 'M', NULL, creator, retired, retired_by, retire_reason, date_retired, date_created, (SELECT UUID()) AS uuid FROM openmrs_b2.users;
-
-UPDATE openmrs_b2.users SET person_id = user_id + @'max_person_id';
- 
-INSERT INTO openmrs_b2.person_name (person_id, middle_name, given_name, family_name, preferred, prefix, creator, voided, voided_by, void_reason, date_voided, date_created, uuid)
-SELECT person_id, username, username, username,  1, NULL, creator, retired, retired_by, retire_reason, date_retired, date_created, (SELECT UUID()) AS uuid FROM openmrs_b2.users;
-
-/* Update patient programs */
-INSERT INTO openmrs_b2.patient_program (patient_program_id, patient_id, program_id, date_enrolled, date_completed, creator, voided, voided_by, void_reason, date_voided, date_created, uuid)
-SELECT patient_program_id, patient_id, program_id, date_enrolled, date_completed, creator, voided, voided_by, void_reason, date_voided, date_created, (SELECT UUID()) AS uuid FROM openmrs_bart1.patient_program WHERE patient_id IN (SELECT patient_id FROM openmrs_bart1.patient);
-
-UPDATE openmrs_b2.patient_program program SET program.date_enrolled = IFNULL((SELECT dates.start_date FROM openmrs_bart1.patient_start_dates dates WHERE program.patient_id = dates.patient_id), program.date_created);
-
-INSERT INTO openmrs_b2.patient_state (patient_program_id, state, start_date, creator, voided, voided_by, void_reason, date_voided, date_created, uuid)
-SELECT patient_program_id, 1, date_enrolled, creator, voided, voided_by, void_reason, date_voided, date_created, (SELECT UUID()) AS uuid FROM openmrs_b2.patient_program WHERE program_id = 1;
-
-INSERT INTO openmrs_b2.patient_state (patient_program_id, state, start_date, creator, voided, voided_by, void_reason, date_voided, date_created, uuid)
-SELECT patient_program_id, 18, date_enrolled, creator, voided, voided_by, void_reason, date_voided, date_created, (SELECT UUID()) AS uuid FROM openmrs_b2.patient_program WHERE program_id = 2;
+/*update the person table in to replace the values of */
+UPDATE openmrs_bart2.person
+SET voided = 0, void_reason = '';
 
 
-INSERT INTO openmrs_b2.relationship (person_a, relationship, person_b, creator, date_created, voided, voided_by, date_voided, void_reason, uuid)
-SELECT p1.patient_id, 13, p2.patient_id, creator, date_created, voided, voided_by, date_voided, void_reason, (SELECT UUID()) AS uuid FROM openmrs_bart1.relationship rel LEFT JOIN openmrs_bart1.person p1 ON rel.person_id = p1.person_id LEFT JOIN openmrs_bart1.person p2 ON rel.relative_id = p2.person_id;
+/* ---------Update the healthdata.MasterPatientRecord ----Reverting to the old format */
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
+UPDATE healthdata.MasterPatientRecord
+SET Birth_Date = CONCAT_WS('-', SUBSTRING(Birth_Date,9,2),CASE SUBSTRING(Birth_Date,6,2) WHEN '01' THEN 'JAN' WHEN '02' THEN 'FEB' WHEN '03' THEN 'MAR' WHEN '04' THEN 'APR' WHEN '05' THEN 'MAY' WHEN '06' THEN 'JUN' WHEN '07' THEN 'JUL' WHEN '08' THEN 'AUG' WHEN '09' THEN 'SEP' WHEN '10' THEN 'OCT' WHEN '11' THEN 'NOV' WHEN '12' THEN 'DEC' END, SUBSTRING(Birth_Date,1,4)),
+    Date_Reg = CONCAT_WS('-', SUBSTRING(Date_Reg,9,2),CASE SUBSTRING(Date_Reg,6,2) WHEN '01' THEN 'JAN' WHEN '02' THEN 'FEB' WHEN '03' THEN 'MAR' WHEN '04' THEN 'APR' WHEN '05' THEN 'MAY' WHEN '06' THEN 'JUN' WHEN '07' THEN 'JUL' WHEN '08' THEN 'AUG' WHEN '09' THEN 'SEP' WHEN '10' THEN 'OCT' WHEN '11' THEN 'NOV' WHEN '12' THEN 'DEC' END, SUBSTRING(Date_Reg,1,4));
+   
+/*End of update*/
