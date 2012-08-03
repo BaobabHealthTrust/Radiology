ecebcb11e12cb8120e9ce888bd2ae99a1ee05831
Blessings <blessings.mhango@gmail.com>
2012-08-03 13:47:31 +0200

    added and modified the search method

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -77,9 +77,9 @@ class PeopleController < GenericPeopleController
       redirect_to :action => "index"
     end
   end
-   
+
   def find_by_exam_number                                                       
-    if request.post?                                                            
+    if request.post?
       index = 0 ; last_exam_num = params[:exam_number]                          
       last_exam_num.each_char do | c |                                          
         next if c == 'R'                                                        
@@ -88,7 +88,7 @@ class PeopleController < GenericPeopleController
       end unless last_exam_num.blank?                                           
                                                                                 
       exam_number = 'R' + (last_exam_num[index..-1].to_s.rjust(8,'0'))          
-                                                                                
+
       order = Order.find(:first,:conditions =>["accession_number = ? AND voided = 0",exam_number])
 
       if order.blank?
@@ -101,21 +101,52 @@ class PeopleController < GenericPeopleController
       end                                                                      
     end                                                                         
   end
+  def search
+		found_person = nil
+		if params[:identifier]
+      radiology_character = params[:identifier][0].chr
+      if radiology_character == "R"
+         order = Order.find(:first,:conditions =>["accession_number = ? AND voided = 0",params[:identifier]])
+         if order
+           unless order.date_created.to_date == Date.today
+             session[:datetime] = order.date_created.to_date
+           end
+           redirect_to :controller => 'patients', :action => 'show',:patient_id => order.patient_id,
+                       :encounter_date => order.date_created.to_date,:examination_number => order.accession_number and return
+         else
+           redirect_to :controller => 'clinic'
+         end
+      end
+			local_results = PatientService.search_by_identifier(params[:identifier])
+
+			if local_results.length > 1
+				@people = PatientService.person_search(params)
+			elsif local_results.length == 1
+				found_person = local_results.first
+			else
+				# TODO - figure out how to write a test for this
+				# This is sloppy - creating something as the result of a GET
+				if create_from_remote
+					found_person_data = PatientService.find_remote_person_by_identifier(params[:identifier])
+					found_person = PatientService.create_from_form(found_person_data['person']) unless found_person_data.nil?
+				end
+			end
+			if found_person
+				if params[:relation]
+					redirect_to search_complete_url(found_person.id, params[:relation]) and return
+				else
+					redirect_to :action => 'confirm', :found_person_id => found_person.id, :relation => params[:relation] and return
+				end
+		  end
+		@relation = params[:relation]
+		@people = PatientService.person_search(params)
+		@patients = []
+		@people.each do | person |
+			patient = PatientService.get_patient(person) rescue nil
+			@patients << patient
+		end
+
+	 end
+
+  end
 end
-  
-
-  
-
-  
-
-
-  
-
-  
-  
-  
-
-  
-  
-
- 
