2ab80782144aa0ca65941d0d6e862ebb49794def
Blessings <blessings.mhango@gmail.com>
2012-08-13 08:04:29 +0200

    removed report button

--- app/controllers/clinic_controller.rb
+++ app/controllers/clinic_controller.rb
@@ -2,7 +2,6 @@ class ClinicController < GenericClinicController
 
   def reports_tab
     @reports = [
-      ["Revenue", "/people/date_select?id=revenue_collected"],
       ["Film(s) used", "/people/date_select?id=film_used"],
       ["Investigations","/people/date_select?id=investigations"]
     ]
