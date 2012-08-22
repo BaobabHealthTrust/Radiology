72b92a126a464921931aeb056052be12325879c9
Blessings <blessings.mhango@gmail.com>
2012-08-22 11:07:08 +0200

    added button for printing recent examinations

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -3,38 +3,18 @@ class PatientsController < GenericPatientsController
   def personal
     @links = []
     patient = Patient.find(params[:id])
+    order = Order.find(:first,:conditions => ["patient_id = ?",patient.id],:order => "start_date DESC")
     
     if use_user_selected_activities
       @links << ["Change User Activities","/user/activities/#{current_user.id}?patient_id=#{patient.id}"]
     end
-    @links << ["National ID (Print)","/patients/dashboard_print_national_id/#{patient.id}"]
-    @links << ["Investigation (Print)","/patients/dashboard_print_visit/#{patient.id}"]
-    
+    @links << ["National ID","/patients/dashboard_print_national_id/#{patient.id}"]
+    unless order.nil?
+    @links << ["Examination Label","/orders/examination_number?order_id=#{order.order_id}"]
+    end
     render :template => 'dashboards/personal_tab', :layout => false
   end
   
-  def patient_visit_label(patient, date = Date.today)
-    label = ZebraPrinter::StandardLabel.new
-    label.font_size = 3
-    label.font_horizontal_multiplier = 1
-    label.font_vertical_multiplier = 1
-    label.left_margin = 50
-    encs = patient.encounters.find(:all,:conditions =>["DATE(encounter_datetime) = ?",date])
-    return nil if encs.blank?
-
-    label.draw_multi_text("Investigation: #{encs.first.encounter_datetime.strftime("%d/%b/%Y %H:%M")}", :font_reverse => true)
-    encs.each {|encounter|
-      encounter.to_s.split("<b>").each do |string|
-        concept_name = string.split("</b>:")[0].strip rescue nil
-        obs_value = string.split("</b>:")[1].strip rescue nil
-        next if string.match(/Workstation location/i)
-        next if obs_value.blank?
-        label.draw_multi_text("#{encounter.name.humanize} - #{concept_name}: #{obs_value}", :font_reverse => false)
-      end
-    }
-    label.print(1)
-  end
-
   def examination
     exam_number = params[:examination_number]
     @order = Order.find(:first,:conditions => ["accession_number = ? AND voided = 0",exam_number])
