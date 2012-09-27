7d59d6c3234828581f84cd437987ca2cdd717cf5
blessings <blessings.mhango@gmail.com>
2012-09-27 12:42:15 +0200

    updated search by identifier method to pick ids with dashes

--- lib/patient_service.rb
+++ lib/patient_service.rb
@@ -1139,11 +1139,19 @@ people = Person.find(:all, :include => [{:names => [:person_name_code]}, :patien
   end
   
   def self.search_by_identifier(identifier)
-    people = PatientIdentifier.find_all_by_identifier(identifier).map{|id| 
+    identifier_without_dashes = identifier.gsub("-","").strip
+    people = PatientIdentifier.find(:all ,
+    :conditions =>["identifier = ? OR identifier =?", identifier,identifier_without_dashes]).map{|id|
       id.patient.person
     } unless identifier.blank? rescue nil
     return people unless people.blank?
 
+=begin
+    people = PatientIdentifier.find_all_by_identifier(identifier).map{|id| 
+      id.patient.person
+    } unless identifier.blank? rescue nil
+    return people unless people.blank?
+=end
     create_from_dde_server = CoreService.get_global_property_value('create.from.dde.server').to_s == "true" rescue false
     if create_from_dde_server 
       dde_server = GlobalProperty.find_by_property("dde_server_ip").property_value rescue ""
