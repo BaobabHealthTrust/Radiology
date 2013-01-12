5dfde8c735415b74da28da5fab2bfbae7eaf71c8
blessings <blessings.mhango@gmail.com>
2013-01-12 20:36:52 +0200

    updated search methods

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -22,7 +22,7 @@ class PeopleController < GenericPeopleController
     render :layout => 'menu'
 
   end
-  
+=begin
   def create
     success = false
     Person.session_datetime = session[:datetime].to_date rescue Date.today
@@ -77,7 +77,7 @@ class PeopleController < GenericPeopleController
       redirect_to :action => "index"
     end
   end
-
+=end
   def find_by_exam_number                                                       
     if request.post?
       index = 0 ; last_exam_num = params[:exam_number]                          
@@ -153,20 +153,7 @@ def search
   def search
 		found_person = nil
 		if params[:identifier]
-
-      if  params[:identifier].length == 9 && params[:identifier][0].chr == "R"
-         order = Order.find(:first,:conditions =>["accession_number = ? AND voided = 0",params[:identifier]])
-         if order
-           session[:examination_number] = order.accession_number
-           redirect_to :controller => 'patients', :action => 'show',:patient_id => order.patient_id,
-                       :encounter_date => order.date_created.to_date,:examination_number => order.accession_number and return
-         else
-           redirect_to :controller => 'clinic'
-         end
-      end
-
 			local_results = PatientService.search_by_identifier(params[:identifier])
-
 			if local_results.length > 1
 				@people = PatientService.person_search(params)
 			elsif local_results.length == 1
@@ -180,30 +167,98 @@ def search
 				end
 			end
 			if found_person
+
         patient = DDEService::Patient.new(found_person.patient)
-        patient.check_old_national_id(params[:identifier])
+
+        national_id_replaced = patient.check_old_national_id(params[:identifier])
+
 				if params[:relation]
 					redirect_to search_complete_url(found_person.id, params[:relation]) and return
-				else
+        elsif national_id_replaced
+          print_and_redirect("/patients/national_id_label?patient_id=#{found_person.id}", next_task(found_person.patient)) and return
+        else
 					redirect_to :action => 'confirm', :found_person_id => found_person.id, :relation => params[:relation] and return
 				end
 			end
 		end
-
-		records_per_page = CoreService.get_global_property_value('records_per_page') || 5
 		@relation = params[:relation]
 		@people = PatientService.person_search(params)
-		@patients = []
+    @search_results = {}
+    @patients = []
 
-    unless @people.nil?
-			@current_page = @people.paginate(:page => params[:page], :per_page => records_per_page.to_i)
-		end
+    (PatientService.search_from_remote(params) || []).each do |data|
+			results = PersonSearch.new(data["npid"]["value"])
+      results.national_id = data["npid"]["value"]
+      results.current_residence =data["person"]["data"]["addresses"]["city_village"]
+      results.person_id = 0
+      results.home_district = data["person"]["data"]["addresses"]["state_province"]
+      results.traditional_authority =  data["person"]["data"]["addresses"]["county_district"]
+      results.name = data["person"]["data"]["names"]["given_name"] + " " + data["person"]["data"]["names"]["family_name"]
+      gender = data["person"]["data"]["gender"]
+      results.occupation = data["person"]["data"]["occupation"]
+      results.sex = (gender == 'M' ? 'Male' : 'Female')
+      results.birthdate_estimated = (data["person"]["data"]["birthdate_estimated"]).to_i
+      results.birth_date = birthdate_formatted((data["person"]["data"]["birthdate"]).to_date , results.birthdate_estimated)
+      results.birthdate = (data["person"]["data"]["birthdate"]).to_date
+      results.age = cul_age(results.birthdate.to_date , results.birthdate_estimated)
+      @search_results[results.national_id] = results
+    end if create_from_dde_server
 
-		@current_page.each do | person |
+
+		(@people || []).each do | person |
 			patient = PatientService.get_patient(person) rescue nil
-			@patients << patient
+      next if patient.blank?
+			results = PersonSearch.new(patient.national_id || patient.patient_id)
+      results.national_id = patient.national_id
+      results.birth_date = patient.birth_date
+      results.current_residence = patient.current_residence
+      results.guardian = patient.guardian
+      results.person_id = patient.person_id
+      results.home_district = patient.home_district
+      results.traditional_authority = patient.traditional_authority
+      results.mothers_surname = patient.mothers_surname
+      results.dead = patient.dead
+      results.arv_number = patient.arv_number
+      results.eid_number = patient.eid_number
+      results.pre_art_number = patient.pre_art_number
+      results.name = patient.name
+      results.sex = patient.sex
+      results.age = patient.age
+      @search_results.delete_if{|x,y| x == results.national_id}
+      @patients << results
 		end
 
+		(@search_results || {}).each do |npid , data |
+      @patients << data
+    end
 	end
 
+  protected
+
+  def cul_age(birthdate , birthdate_estimated , date_created = Date.today, today = Date.today)
+
+    # This code which better accounts for leap years
+    patient_age = (today.year - birthdate.year) + ((today.month - birthdate.month) + ((today.day - birthdate.day) < 0 ? -1 : 0) < 0 ? -1 : 0)
+
+    # If the birthdate was estimated this year, we round up the age, that way if
+    # it is March and the patient says they are 25, they stay 25 (not become 24)
+    birth_date = birthdate
+    estimate = birthdate_estimated == 1
+    patient_age += (estimate && birth_date.month == 7 && birth_date.day == 1  &&
+        today.month < birth_date.month && date_created.year == today.year) ? 1 : 0
+  end
+
+  def birthdate_formatted(birthdate,birthdate_estimated)
+    if birthdate_estimated == 1
+      if birthdate.day == 1 and birthdate.month == 7
+        birthdate.strftime("??/???/%Y")
+      elsif birthdate.day == 15
+        birthdate.strftime("??/%b/%Y")
+      elsif birthdate.day == 1 and birthdate.month == 1
+        birthdate.strftime("??/???/%Y")
+      end
+    else
+      birthdate.strftime("%d/%b/%Y")
+    end
+  end
 end
