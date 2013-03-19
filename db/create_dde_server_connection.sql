1250874be952d6dfc15fad2eb2f9e350b572f7df
blessings <blessings.mhango@gmail.com>
2013-03-19 11:38:08 +0200

    updated metadata and dde connection script

--- db/create_dde_server_connection.sql
+++ db/create_dde_server_connection.sql
@@ -1,6 +1,6 @@
 DELETE FROM global_property WHERE property = 'dde_server_ip';
 
-INSERT INTO global_property (property, property_value, `description`, uuid) VALUES ('dde_server_ip', '192.168.6.139:3001', 'Demographics Data Exchange Server IP address and port', (SELECT UUID()));
+INSERT INTO global_property (property, property_value, `description`, uuid) VALUES ('dde_server_ip', 'localhost:3001', 'Demographics Data Exchange Server IP address and port', (SELECT UUID()));
 
 DELETE FROM global_property WHERE property = 'dde_server_username';
 
@@ -9,4 +9,8 @@ INSERT INTO global_property (property, property_value, `description`, uuid) VALU
 DELETE FROM global_property WHERE property = 'dde_server_password';
 
 INSERT INTO global_property (property, property_value, `description`, uuid) VALUES ('dde_server_password', 'admin', 'Demographics Data Exchange Server password', (SELECT UUID()));
+
+DELETE FROM global_property WHERE property = 'create.from.dde.server';
+
+INSERT INTO global_property (property, property_value, `description`, uuid) VALUES ('create.from.dde.server', 'false', 'Demographics Data Exchange connection parameter', (SELECT UUID()));
 
