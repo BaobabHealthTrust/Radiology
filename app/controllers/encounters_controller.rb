3c457e64945b8fc4be6605e87c43a26f85c88d36
Blessings <blessings.mhango@gmail.com>
2014-06-03 12:02:51 +0200

    print appointments

--- app/controllers/encounters_controller.rb
+++ app/controllers/encounters_controller.rb
@@ -16,7 +16,9 @@ class EncountersController < GenericEncountersController
         appointment_encounter.void("Given a new appointment date")
       end
     end
-    	
+    
+    radiology_type =  params[:observations][0][:value_coded_or_text] rescue nil
+     	
     if params['encounter']['encounter_type_name'].upcase == "APPOINTMENT"
       observations = []
       (params[:observations] || []).each do |observation|
@@ -67,8 +69,7 @@ class EncountersController < GenericEncountersController
     #create observations for the just created encounter
     create_obs(encounter , params)
 
-		
-    
+
     unless params[:location]
     #find a way of printing the lab_orders labels
      if params['encounter']['encounter_type_name'] == "LAB ORDERS"
@@ -79,7 +80,7 @@ class EncountersController < GenericEncountersController
       if params['encounter']['encounter_type_name'].to_s.upcase == "APPOINTMENT" && !params[:report_url].nil? && !params[:report_url].match(/report/).nil?
          redirect_to  params[:report_url].to_s and return
       elsif params['encounter']['encounter_type_name'].upcase == 'APPOINTMENT'
-        print_and_redirect("/patients/dashboard_print_visit/#{params[:encounter]['patient_id']}","/patients/show/#{params[:encounter]['patient_id']}")
+        appointment_print(encounter,radiology_type)
         return
       end
       redirect_to next_task(@patient)
@@ -95,4 +96,80 @@ class EncountersController < GenericEncountersController
       #return encounter.id.to_s  # support non-RESTful creation of encounters
     end
   end
+
+  def appointment
+    print_string = appointment_label(params)
+    send_data(print_string,:type=>"application/label; charset=utf-8",:stream=> false, 
+      :filename=>"#{params[:encounter_id]}#{rand(10001)}.lbl",:disposition => "inline")
+  end
+
+  def appointment_print(encounter,radiology_type)
+    print_and_redirect("/encounters/appointment?encounter_id=#{encounter.id}&rad_type=#{radiology_type}", "/patients/show/#{encounter.patient_id}")
+  end
+  
+  def appointment_label(encounter_id)
+    encounter = Encounter.find(params["encounter_id"])
+    encounter_id = encounter.id
+
+    examination_concept = ConceptName.find_by_name("EXAMINATION").concept_id
+    examination_obs = Observation.find( :first, :select => "concept_id, value_coded",
+                                             :conditions => ["encounter_id = ? AND concept_id = ?",
+                                               encounter_id, examination_concept])
+    examination = examination_obs.answer_concept.shortname rescue ''
+    
+    if examination.blank?
+      examination = examination_obs.answer_concept.fullname rescue ''
+    end
+
+    detailed_examination_concept = ConceptName.find_by_name("DETAILED EXAMINATION").concept_id
+    detailed_examination_obs = Observation.find( :first, :select => "concept_id, value_coded",
+                                           :conditions => ["encounter_id = ? AND concept_id = ?",
+                                             encounter_id, detailed_examination_concept])
+    detailed_examination = detailed_examination_obs.answer_concept.shortname rescue nil
+    if detailed_examination.blank?
+      detailed_examination = detailed_examination_obs.answer_concept.fullname rescue nil
+    end
+     
+    appointment_concept = ConceptName.find_by_name("APPOINTMENT DATE").concept_id
+    appointment_date_obs = Observation.find( :first, :select => "concept_id, value_datetime",
+                                             :conditions => ["encounter_id = ? AND concept_id = ?",
+                                               encounter_id,  appointment_concept])
+    appointment_date = appointment_date_obs.value_datetime.strftime('%A, %d %b %Y')
+    
+    if params["rad_type"]
+       radiology_type = ConceptName.find_by_name(params["rad_type"])
+       type = radiology_type.concept.shortname
+       type = radiology_type.concept.fullname if type.blank?
+    end 
+
+    patient_bean = PatientService.get_patient(encounter.patient.person)
+    return unless patient_bean.national_id
+    sex =  patient_bean.sex.match(/F/i) ? "(F)" : "(M)"
+    address = patient.person.address.strip[0..24].humanize rescue ""
+    session_date = encounter.encounter_datetime.strftime('%d-%b-%Y')
+    
+    
+    label = ZebraPrinter::StandardLabel.new
+    label.font_size = 4
+    label.x = 50
+    label.font_horizontal_multiplier = 1
+    label.font_vertical_multiplier = 1
+    label.left_margin = 100
+    label.draw_barcode(150,220,0,1,5,15,90,false,"#{patient_bean.national_id}")
+    label.draw_multi_text("#{patient_bean.name.titleize} #{sex} ")
+    label.draw_multi_text("#{patient_bean.national_id_with_dashes} DOB #{patient_bean.birth_date}")
+   
+    label.draw_multi_text("Appointment : #{type}")
+    
+    unless detailed_examination.blank?
+      label.draw_multi_text("Examination : #{examination}-#{detailed_examination}")
+    else
+      label.draw_multi_text("Examination : #{examination}")
+    end
+
+    label.draw_multi_text("Appointment Date : #{appointment_date}")
+   
+    label.print(1)
+  end
+
 end
