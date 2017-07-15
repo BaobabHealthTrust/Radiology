3aa010f4af493e12b3260ec8786bdaaea9025eec
Blessings <blessings.mhango@gmail.com>
2017-07-15 17:50:21 +0200

    birthdate

--- lib/dde2_service.rb
+++ lib/dde2_service.rb
@@ -422,18 +422,18 @@ module DDE2Service
   end
 
   def self.update_demographics(patient_bean)
-    birth_date = nil
+    birthdate = nil
     if patient_bean.birthdate_estimated.present? && patient_bean.birthdate_estimated == 1
       if patient_bean.birth_date.split("/").second == "???"
-        birth_date = Date.new(patient_bean.birth_date.split("/").third.to_i,7,1).strftime("%Y-%m-%d")
+        birthdate = Date.new(patient_bean.birth_date.split("/").third.to_i,7,1).strftime("%Y-%m-%d")
       elsif patient_bean.birth_date.split("/").first == "??"
-        birth_date = Date.new(patient_bean.birth_date.split("/").third.to_i,
+        birthdate = Date.new(patient_bean.birth_date.split("/").third.to_i,
                              patient_bean.birth_date.split("/").second.to_i,1)..strftime("%Y-%m-%d")
       else
-        birth_date = patient_bean.birth_date.to_date.strftime("%Y-%m-%d")
+        birthdate = patient_bean.birth_date.to_date.strftime("%Y-%m-%d")
       end  
     else   
-      birth_date = patient_bean.birth_date.to_date.strftime("%Y-%m-%d")
+      birthdate = patient_bean.birth_date.to_date.strftime("%Y-%m-%d")
     end
 
     result = {
@@ -447,7 +447,7 @@ module DDE2Service
             "citizenship" => (patient_bean.citizenship rescue ""),
             "country_of_residence" => (patient_bean.country_of_residence rescue ""),
         },
-        "birthdate"=> birth_date,
+        "birthdate"=> birthdate,
         "birthdate_estimated" => (patient_bean.birthdate_estimated == '0' ? false : true),
         "current_residence"=> patient_bean.landmark,
         "current_village"=> patient_bean.current_residence,
